32df98c6cf61456ec99388e7cd7c5d75a0929a9d
Yoyas24XD <diego24xp37@gmail.com>
2025-07-13 21:30:53 +0200

    Update button colors and enhance divider component with vertical support

--- src/components/ui/divider.tsx
+++ src/components/ui/divider.tsx
@@ -1,3 +1,28 @@
-export const Divider = () => {
-  return <div className="border-t border-gray-300 my-4" />;
+import type { FC } from "react";
+
+export const Divider: FC<{ vertical?: boolean }> = ({ vertical = false }) => {
+  if (vertical) {
+    return (
+      <hr
+        className="
+          border-l-2
+          border-gray-300
+          dark:border-gray-600
+          h-full
+          mx-2
+          min-h-[30px]"
+      />
+    );
+  }
+  return (
+    <hr
+      className="
+        border-t-2
+        border-gray-300
+        dark:border-gray-600
+        my-4
+        w-full
+      "
+    />
+  );
 };
