d7777242266601e9f51040103df484373b6e236c
Yoyas24XD <diego24xp37@gmail.com>
2025-07-21 16:01:40 +0200

    bugfix: update useHpLine hook to use CarSetup type instead of InitialDataContextProps

--- src/hooks/use-hp-line.ts
+++ src/hooks/use-hp-line.ts
@@ -1,7 +1,7 @@
 import { useMemo } from "react";
-import type { InitialDataContextProps } from "./use-initial-data";
+import type { CarSetup } from "./use-car-setup";
 
-export const useHpLine = (data: InitialDataContextProps["data"]) => {
+export const useHpLine = (data: CarSetup["data"]) => {
   return useMemo(() => {
     return data.map(({ rpm, torque }) => ((rpm * torque) / 9549) * 1.34102);
   }, [data]);
